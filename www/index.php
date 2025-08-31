e2a8d65712a3c046e6748de0cbbc3d83ac0b1b1b
Alfred Cerny <github@turtle.quest>
2025-08-31 09:04:35 +0200

    🚀 feat: refactor bootstrap process in index.php 🔧 fix: update application booting method ✨ chore: improve code readability and structure

--- www/index.php
+++ www/index.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-exit(WaymarkTo\Bootstrap::boot()
-	->createContainer()
-	->getByType(Contributte\Console\Application::class)
-	->run());
+$bootstrap = new WaymarkTo\Bootstrap;
+$container = $bootstrap->bootWebApplication();
+$application = $container->getByType(Nette\Application\Application::class);
+$application->run();
